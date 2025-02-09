f41c9d210feedb012d40c2b49f47e0419022283e
OJarrisonn <j.h.m.t.v.10@gmail.com>
2025-02-09 19:05:00 -0300

    v2.1: configure nix lang with nixd
    Signed-off-by: OJarrisonn <j.h.m.t.v.10@gmail.com>

--- lua/config/custom/langs.lua
+++ lua/config/custom/langs.lua
@@ -1,2 +1,23 @@
-local nls = require("null-ls")
+local nls = require "null-ls"
+local lspconfig = require "lspconfig"
 
+nls.register {
+    nls.builtins.formatting.alejandra,
+}
+
+lspconfig.nixd.setup {
+    cmd = { "nixd" },
+    filetypes = { "nix" },
+    settings = {
+        nixd = {
+            nixpkgs = {
+                expr = "import <nixpkgs> {}",
+            },
+            options = {
+                home_manager = {
+                    expr = "(builtins.getFlake \"/home/ojarrisonn_/.config/home-manager\").homeConfigurations.\"ojarrisonn_\".options",
+                },
+            },
+        },
+    },
+}
